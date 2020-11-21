5b13887b42ae50ec2ef38e126bdcdc6b373080e6
dima <dmytro.martyniuk09@gmail.com>
2020-11-21 16:29:12 +0100

    Added template config + 2 html files

--- src/main/java/guru/springframework/spring5webapp/entity/Book.java
+++ src/main/java/guru/springframework/spring5webapp/entity/Book.java
@@ -61,6 +61,14 @@ public class Book {
         this.authors = authors;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
